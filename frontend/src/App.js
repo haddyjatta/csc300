0289948c50e5b8f9c920bdeaa6ad836b538d483f
haddyjatta <h_jatta@salemstate.edu>
2024-01-25 13:18:02 -0500

    First Assignment 01/25/24

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,6 +3,8 @@ import React from "react";
 import { Route, Routes } from "react-router-dom";
 
 // We import all the components we need in our app
+import MbtaRoutesPage from "./components/pages/mbtaRoutes";
+import MbtaAlertsPage from "./components/pages/mbtaAlerts";
 import Navbar from "./components/navbar";
 import LandingPage from "./components/pages/landingPage";
 import HomePage from "./components/pages/homePage";
@@ -27,6 +29,8 @@ const App = () => {
       <Navbar />
       <UserContext.Provider value={user}>
         <Routes>
+          <Route exact path="/mbtaRoutes" element={<MbtaRoutesPage />} />
+          <Route exact path="/mbtaAlerts" element={<MbtaAlertsPage />} />  
           <Route exact path="/" element={<LandingPage />} />
           <Route exact path="/home" element={<HomePage />} />
           <Route exact path="/login" element={<Login />} />
